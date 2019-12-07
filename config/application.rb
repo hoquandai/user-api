a9cdefb5d899f33f5e4e19e6e836af36bf21a89f
Dai Ho <daiho@teqnological.asia>
2019-12-07 18:12:16 +0700

    allow cors

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module Uapi
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS PUT DELETE}.join(",")
+  }
   end
 end
