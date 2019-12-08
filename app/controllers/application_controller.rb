aba181c7a369ddee85724a88a887eb65eaecb926
Dai Ho <daiho@teqnological.asia>
2019-12-08 13:03:24 +0700

    authorixe user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
+# :nodoc
 class ApplicationController < ActionController::API
+  before_action :authorize
+
   def render_serializer(resource, serializer)
     render json: serializer.new(resource).serialized_json, status: 200
+  end
+
+  protected
+
+  def authorize
+    if params[:token].present?
+      unless User.find_by(token: params[:token])
+        render json: { error: 'Please login', status: 400 }
+      end
+    else
+      render json: { error: 'Please send token', status: 400 }
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_135804) do
+ActiveRecord::Schema.define(version: 2019_12_08_050955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "dob"
+    t.string "gender"
+    t.string "phone"
+    t.string "city"
+    t.string "name"
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,6 +41,8 @@ ActiveRecord::Schema.define(version: 2019_12_03_135804) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
+    t.string "password_digest"
+    t.string "token"
   end
 
 end
