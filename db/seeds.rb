25e1057d76949506ad6c9181e23b130c130adabb
Dai Ho <daiho@teqnological.asia>
2019-12-22 14:13:34 +0700

    clear seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'Ho Quan Dai', email: 'hoquandai@example.com', dob: '1998-08-08', phone: '033444987', gender: 'male', city: 'HCM', password: '12345678')
-User.create(name: 'Le Thanh Thanh Toai', email: 'llthanhtoai@example.com', dob: '1998-11-11', phone: '033444987', gender: 'male', city: 'HCM', password: '12345678')
-User.create(name: 'Do Nhat Tien', email: 'dntien@example.com', dob: '1998-09-09', phone: '033444987', gender: 'male', city: 'HCM', password: '12345678')
+# User.create(name: 'Ho Quan Dai', email: 'hoquandai@example.com', dob: '1998-08-08', phone: '033444987', gender: 'male', city: 'HCM', password: '12345678')
+# User.create(name: 'Le Thanh Thanh Toai', email: 'llthanhtoai@example.com', dob: '1998-11-11', phone: '033444987', gender: 'male', city: 'HCM', password: '12345678')
+# User.create(name: 'Do Nhat Tien', email: 'dntien@example.com', dob: '1998-09-09', phone: '033444987', gender: 'male', city: 'HCM', password: '12345678')
 
-Skill.create(name: 'Math', desc: 'Math skills')
-Skill.create(name: 'Physic', desc: 'Physic skills')
+# Skill.create(name: 'Math', desc: 'Math skills')
+# Skill.create(name: 'Physic', desc: 'Physic skills')
 
-Contract.create(course: 'Class 8', subject: 'English', addr: 'HCM', schedule: 'Mon, Thurs', time: '3.5', status: 'Waiting', paid: false, tutor_id: 1, student_id: 2)
+# Contract.create(course: 'Class 8', subject: 'English', addr: 'HCM', schedule: 'Mon, Thurs', time: '3.5', status: 'Waiting', paid: false, tutor_id: 1, student_id: 2)
