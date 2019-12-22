92783b17051fb5289af4e2aa6e4ddfac4ef4e853
Dai Ho <daiho@teqnological.asia>
2019-12-22 14:00:10 +0700

    serialize price and desc for user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :email, :dob, :gender, :phone, :city, :skills
+  attributes :name, :email, :dob, :gender, :phone, :city, :skills, :price, :desc
 
   attributes :image do |user|
     user.image.url if user.image.present?
