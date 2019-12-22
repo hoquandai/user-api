52d0289b9868bd01568293f4b3e5d0c487245a8e
Dai Ho <daiho@teqnological.asia>
2019-12-22 14:53:10 +0700

    desc price prams

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,7 +63,7 @@ class UsersController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
     params.require(:user).permit(
-      :name, :password, :email, :dob, :gender, :phone, :city, :image_data_uri, skills: []
+      :name, :password, :email, :dob, :gender, :phone, :city, :image_data_uri, :desc, :price, skills: []
     )
   end
 end
